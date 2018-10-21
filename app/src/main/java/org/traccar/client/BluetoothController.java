9df8701bfd6c5f4feb5224212adcf77a55cd4f3d
seet88 <cokaco88@gmail.com>
2018-10-21 12:36:44 +0200

    init - moved bt search class, compile ok

---
+++ app/src/main/java/org/traccar/client/BluetoothController.java
@@ -0,0 +1,74 @@
+package org.traccar.client;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class BluetoothController {
+    private final static String TAG = MainActivity.class.getSimpleName();
+
+    public static final int REQUEST_ENABLE_BT = 1;
+    public static final int BTLE_SERVICES = 2;
+
+    private HashMap<String, BTLE_Device> mBTDevicesHashMap;
+    private ArrayList<BTLE_Device> mBTDevicesArrayList;
+    private Context context;
+
+    private Button scan;
+    private Button refresh;
+    private BroadcastReceiver_BTState mBTStateUpdateReceiver;
+    private Scanner_BTLE mBTLeScanner;
+    public BluetoothController(Context context) {
+        this.context = context;
+        if (!context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            //Toast.makeText(getApplicationContext(), "BLE not supported", Toast.LENGTH_SHORT).show();
+            Utils.toast(context, "BLE not supported");
+        }
+        mBTStateUpdateReceiver = new BroadcastReceiver_BTState(context);
+        mBTLeScanner = new Scanner_BTLE(context, 15000, -175);
+        mBTDevicesHashMap = new HashMap<>();
+        mBTDevicesArrayList = new ArrayList<>();
+
+        context.registerReceiver(mBTStateUpdateReceiver, new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED));
+    }
+
+    public void addDevice(BluetoothDevice device, int rssi) {
+
+        String address = device.getAddress();
+        if (!mBTDevicesHashMap.containsKey(address)) {
+            BTLE_Device btleDevice = new BTLE_Device(device);
+            btleDevice.setRSSI(rssi);
+
+            mBTDevicesHashMap.put(address, btleDevice);
+            mBTDevicesArrayList.add(btleDevice);
+        }
+        else {
+            mBTDevicesHashMap.get(address).setRSSI(rssi);
+        }
+
+        Utils.toast(context, "AddDevice:"+address);
+        //adapter.notifyDataSetChanged();
+    }
+
+    public void startScan(){
+        scan.setText("Scanning...");
+
+        mBTDevicesArrayList.clear();
+        mBTDevicesHashMap.clear();
+        //adapter.notifyDataSetChanged();
+
+        mBTLeScanner.start();
+    }
+
+    public void stopScan() {
+        scan.setText("Scan Again");
+
+        mBTLeScanner.stop();
+    }
+}
---
+++ app/src/main/java/org/traccar/client/Scanner_BTLE.java
@@ -0,0 +1,101 @@
+package org.traccar.client;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.os.Handler;
+
+public class Scanner_BTLE {
+    private BluetoothController bc;
+    private final Context context;
+
+    private BluetoothAdapter mBluetoothAdapter;
+    private boolean mScanning;
+    private Handler mHandler;
+
+    private long scanPeriod;
+    private int signalStrength;
+
+    public Scanner_BTLE(Context context, long scanPeriod, int signalStrength) {
+        this.context = context;
+
+        mHandler = new Handler();
+
+        this.scanPeriod = scanPeriod;
+        this.signalStrength = signalStrength;
+
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) context.getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+    }
+
+    public boolean isScanning() {
+        return mScanning;
+    }
+
+    public void start() {
+        if (!Utils.checkBluetooth(mBluetoothAdapter)) {
+            //seet Utils.requestUserBluetooth(context);
+            bc.stopScan();
+        }
+        else {
+            scanLeDevice(true);
+        }
+    }
+
+    public void stop() {
+        scanLeDevice(false);
+    }
+
+    // If you want to scan for only specific types of peripherals,
+    // you can instead call startLeScan(UUID[], BluetoothAdapter.LeScanCallback),
+    // providing an array of UUID objects that specify the GATT services your app supports.
+    private void scanLeDevice(final boolean enable) {
+        if (enable && !mScanning) {
+            Utils.toast(context, "Starting BLE scan...");
+
+            // Stops scanning after a pre-defined scan period.
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Utils.toast(context, "Stopping BLE scan...");
+
+                    mScanning = false;
+                    mBluetoothAdapter.stopLeScan(mLeScanCallback);
+
+                    bc.stopScan();
+                }
+            }, scanPeriod);
+
+            mScanning = true;
+
+            mBluetoothAdapter.startLeScan(mLeScanCallback);
+//            mBluetoothAdapter.startLeScan(uuids, mLeScanCallback);
+        }
+        else {
+            mScanning = false;
+            mBluetoothAdapter.stopLeScan(mLeScanCallback);
+        }
+    }
+
+    // Device scan callback.
+    private BluetoothAdapter.LeScanCallback mLeScanCallback =
+            new BluetoothAdapter.LeScanCallback() {
+
+                @Override
+                public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
+
+                    final int new_rssi = rssi;
+                    if (rssi > signalStrength) {
+                        mHandler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                bc.addDevice(device, new_rssi);
+                                Utils.toast(context, "CallBackdevice"+device.getAddress());
+                            }
+                        });
+                    }
+                }
+            };
+}
